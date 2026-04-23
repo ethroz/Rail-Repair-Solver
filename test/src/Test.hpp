da10bb7e998cad9c55f32af1c62d84959b050bc7
ethroz <48915432+ethroz@users.noreply.github.com>
2026-04-22 23:04:35 -0300

    Inlined the Utils.hpp file into Test.hpp

--- test/src/Test.hpp
+++ test/src/Test.hpp
@@ -41,6 +41,26 @@ protected:
     bool failed = false;
 };
 
+template<typename T>
+concept formattable = requires (T & v, std::format_context ctx) {
+    std::formatter<std::remove_cvref_t<T>>().format(v, ctx);
+};
+
+template<typename T>
+inline std::string toString(const T& val) {
+    if constexpr (formattable<T>) {
+        return std::format("{}", val);
+    }
+    else {
+        std::stringstream ss;
+        ss << "0x" << std::hex;
+        for (int i = 0; i < sizeof(T); ++i) {
+            ss << std::setw(2) << std::setfill('0') << (int)reinterpret_cast<const uint8_t*>(&val)[i];
+        }
+        return ss.str();
+    }
+}
+
 }
 
 // Defines
